1eb061b05ff827d8a3129cfe42cda89987c19b8f
Andrey <farengeit1989@yandex.ru>
2021-03-21 15:47:32 +0700

    Поправил 4-ю задачу

--- lesson_06/task_04.py
+++ lesson_06/task_04.py
@@ -4,11 +4,11 @@
 # и название каждого хобби: преобразовать в какой-нибудь контейнерный тип (список, кортеж, множество, словарь).
 # Обосновать выбор типа. Подумать, какие могут возникнуть проблемы при парсинге. В словаре должны храниться данные,
 # полученные в результате парсинга.
+import json
 
 users = open('users.csv', 'r', encoding='utf8')
 hobby = open('hobby.csv', 'r', encoding='utf8')
-
-life_style = {}
+dict_file = open('life_style.txt', 'w', encoding='utf8')
 
 users_length = 0
 for line in users:
@@ -26,18 +26,42 @@ if hobby_length > users_length:
 users.seek(0)
 hobby.seek(0)
 
+
 for line in users:
-    surname, name, fathers_name = line.strip().split(',')
+    life_style = {}
     if line.strip():
-        user = (surname, name, fathers_name)  # преобразовал в кортеж, т.к. имя у человека меняться вряд ли будет
+        user = ' '.join(line.strip().split(','))
+        # я не стал парсить ФИО, потому что json ругается на ключи контейнерного типа. В теории, можно было бы создать
+        # отдельный файл, где хранились бы кортежи, а здесь в качестве ключа были бы номера строк, на которых они лежат,
+        # но я решил не лезть в такие дебри.
         life_style.setdefault(user, None)
     for line in hobby:
         his_hobby = line.strip().split(',')  # преобразовал в список, т.к. хобби и их количество могут меняться
         if line.strip():
             life_style[user] = his_hobby
         break
+    dict_file.write('\n')
+    dict_file.write(json.dumps(life_style, ensure_ascii=False))
+    dict_file.write('\n')
 
 users.close()
 hobby.close()
+dict_file.close()
 
-print(life_style)
+with open('life_style.txt', 'r', encoding='utf8') as dict_file:
+    life_style_result = {}
+    for line in dict_file:
+        result = json.loads(dict_file.readline())
+        life_style_result.update(result)
+    print(life_style_result, '\n', type(life_style_result))
+
+# Думаю, мне удалось выполнить главное условие задачи - ограничение по ОЗУ. Исходные файлы читаются построчно и
+# результат постепенно добавляется в конечный файл. При получении данных из этого файла, они, опять же, вытаскиваются
+# постепенно и преобразуются в нужный словарь. Таким образом, целиком словарь не загружается в память в процессе работы
+# программы. Единственное, поступился парсингом ФИО. Но, если бы это было так нужно (получить отедльно имя, фамилию и
+# отчество, я бы это делал уже после того, как вытащил словарь обратно, просто распарсив ключи и сделав новый словарь
+# уже в файле с ключами - кортежами.
+
+
+
+
